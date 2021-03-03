b2a8f4b9d35f346af45c2d197a7f456fab223607
Anotnio <lateralprogramming@gmail.com>
2021-03-03 08:03:55 -0800

    why

--- app/models.py
+++ app/models.py
@@ -8,8 +8,21 @@ class User(db.Model):
     username = db.Column(db.String(150), nullable=False, unique=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
+    post = db.relationship('Post', backref='author', lazy=True)
 
     def __init__(self, username, email, password):
         self.username= username
         self.email = email
         self.password = generate_password_hash(password)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    content = db.Column(db.String(300))
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, title, content, user_id):
+        self.title = title
+        self.content = content
+        self.user_id = user_id
