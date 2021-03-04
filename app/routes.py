391e6295be85f057dfb7d5080d5b8366ff70c1b5
Anotnio <lateralprogramming@gmail.com>
2021-03-04 09:38:59 -0800

    falling behind

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
-from app import app
-from flask import render_template, request
-from app.forms import UserInfoForm, PostForm
+from app import app, db, mail, Message
+from flask import render_template, request, flash, redirect, url_for
+from app.forms import UserInfoForm, PostForm, LoginForm
+from app.models import User, Post
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import check_password_hash
 
 @app.route('/')
 @app.route('/index')
-def hello_world():
+def index():
     context = {
         'title': 'Kekambas Blog | HOME',
         'customer_name': 'Brian',
@@ -21,16 +24,62 @@ def register():
         username = form.username.data
         email = form.email.data
         password = form.password.data
-        print(username, email, password)
+        new_user = User(username, email, password)
+        db.session.add(new_user)
+        db.session.commit()
+        # Send email to new user
+        msg = Message(f'Welcome, {username}', [email])
+        msg.body = "Thank you for signing up for the Kekambas_Blog. I hope you enjoy our app!"
+        msg.html = "<p>Thank you so much!</p>"
+        mail.send(msg)
+        # Send email to new user
+        flash("You have successfuly Signed UP!", "success")
+        return redirect(url_for('index'))
     return render_template('register.html', title=title, form=form)
 
 
 @app.route('/createpost', methods=['GET', 'POST'])
+@login_required
 def createpost():
     title = "Kekambas Blog | CREATE POST"
     post = PostForm()
     if request.method == 'POST' and post.validate():
         post_title = post.title.data
         content = post.content.data
-        print(post_title, content)
+        user_id = current_user.id
+        
+        new_post = Post(post_title, content, user_id)
+
+        db.session.add(new_post)
+        db.session.commit()
+
+        flash("You have made a post")
+        return redirect(url_for('createpost'))
+
     return render_template('create_post.html', post=post, title=title)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    title = 'Kekambas Blog | LOGIN'
+    form = LoginForm()
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        password = form.password.data
+        user = User.query.filter_by(username=username).first()
+        if user is None or not check_password_hash(user.password, password):
+            flash("Incorrect email/password. Please try again", 'danger')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        flash("You have logged in", 'success')
+        next_page = request.args.get('next')
+        if next_page:
+            return redirect(url_for(next_page.lstrip('/')))
+        return redirect(url_for('index'))
+    return render_template('login.html', title=title, form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash("You have logged out", 'primary')
+    return redirect(url_for('index'))
